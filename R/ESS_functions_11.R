3458ba04c801a167b19a8ad5b4b41040b1c1f97a
Daniel Falster <daniel.falster@mq.edu.au>
2014-04-30 10:54:12 +1000

    Tidy source

---
+++ R/ESS_functions_11.R
@@ -0,0 +1,277 @@
+# Dan Falster Functions for Optimal Offspring size model last modified Dec 4 2006
+
+TOL <- 10^-9
+DERIV <- 10^-6  # percentage difference when taking numerical derivatives
+
+# returns vector from lo to hi with multiplication steps of incr
+seq.log <- function(lo, hi, incr) {
+    temp <- NULL
+    while (lo < hi) {
+        temp <- c(temp, lo)
+        lo <- lo * incr
+    }
+    if (max(temp) < hi)
+        temp <- c(temp, hi)
+    return(temp)
+}
+
+# offspring production - n estimates offspring production through scaling with
+# body size, n_obs uses observed values for E and RI
+n <- function(w0, lri) {
+    return(lri/w0)
+}
+
+# Offspring size-number tradeoff
+RI <- function(wa) {
+    return(c_RI * wa^b_RI)
+}
+
+# Rerproductive investment as function adult size
+RL <- function(wa) {
+    return(c_RL * wa^b_RL)
+}
+
+# Rerproductive lifespan as function adult size
+LRI <- function(wa) {
+    return(c_LRI * wa^b_LRI)
+}
+
+# size at thinning onset as function offsprinf size, adult size, and lifetime
+# reproductive investment LRI
+wt <- function(w0, wa, lri) {
+    return((1/(s * lri) * wa^r * w0^(1 - q) * D^(k/w0 - q))^(1/(r - q)))
+}
+
+# surival functions
+s_est_phase <- function(w, w0) {
+    return(s * (w/w0)^(-k/w0))
+}
+
+s_densindep_phase <- function(w, w0) {
+    return(s * D^(q - k/w0) * (w0/w)^(q))
+}
+
+s_ST_bound <- function(w, w0, wa, lri) {
+    return((w/wa)^(-r)/n(w0, lri))
+}
+
+# survival to adulthood - for resident only
+s_adult <- function(w0, wa, wt) {
+    return(s * D^(q - k/w0) * wt^(r - q) * w0^q * wa^(-r))
+}
+
+# survival to size w of mutant in env of residents
+s_mut <- function(w, w0.mut, w0.res, wa, lri) {
+    # standradise array dimensions for delaing with plotting etc
+    LEN <- max(length(w), length(w0.mut), length(w0.res), length(wa), length(lri))
+
+    # print(LEN)
+
+    if (length(w) == LEN) {
+        W <- w
+    } else {
+        W <- rep(w[1], LEN)
+    }
+    if (length(w0.mut) == LEN) {
+        W0.MUT <- w0.mut
+    } else {
+        W0.MUT <- rep(w0.mut[1], LEN)
+    }
+    if (length(w0.res) == LEN) {
+        W0.RES <- w0.res
+    } else {
+        W0.RES <- rep(w0.res[1], LEN)
+    }
+    if (length(wa) == LEN) {
+        WA <- wa
+    } else {
+        WA <- rep(wa[1], LEN)
+    }
+    if (length(lri) == LEN) {
+        LRI2 <- lri
+    } else {
+        LRI2 <- rep(lri[1], LEN)
+    }
+
+    # solves for point of thinning onset
+    WT <- wt(W0.RES, WA, LRI2)
+
+    gam <- gamma(W0.MUT, W0.RES, WA, WT)
+
+    WT_mut <- gam * WT * W0.MUT/W0.RES
+
+    surv <- rep(0, times = LEN)
+
+    for (i in 1:LEN) {
+        # check to ensure offspring not larger than adult!
+        if (W[i] > WA[i])
+            surv[i] <- 0 else if (W[i] <= D * W0.MUT[i]) {
+            # establishment phase
+            surv[i] <- s_est_phase(W[i], W0.MUT[i])
+        } else {
+            # excess juveniles - crash at onset of thinning
+            if (WT_mut[i] < D * W0.MUT[i]) {
+                s_crash <- s_ST_bound(D * W0.RES[i], W0.RES[i], WA[i], LRI2[i])/s_est_phase(D *
+                  W0.RES[i], W0.RES[i])  # magnitude of crash
+                surv[i] <- s * D^(-k/W0.MUT[i]) * (s_crash/alpha_ij(W0.MUT[i], W0.RES[i])) *
+                  (W[i]/D/W0.MUT[i])^(-r * alpha_ij(W0.MUT[i], W0.RES[i]))
+            } else if (WT_mut[i] > WA[i]) {
+                # too few juveniles - thinning starts after Wa --> density indept mortality only
+                surv[i] <- s_densindep_phase(W[i], W0.MUT[i])
+            } else {
+                # normal case - thinning starts between D*w0 & Wa
+
+                if (W[i] <= WT_mut[i]) {
+                  surv[i] <- s_densindep_phase(W[i], W0.MUT[i])
+                } else {
+                  surv[i] <- s * D^(-k/W0.MUT[i]) * (gam[i] * WT[i]/D/W0.RES[i])^(-q) *
+                    (W[i] * W0.RES[i]/WT[i]/W0.MUT[i]/gam[i])^(-r * alpha_ij(gam[i] *
+                      W0.MUT[i], W0.RES[i]))
+                }
+            }
+        }
+    }
+    return(surv)
+
+}
+
+# adjustment function for RGR
+gamma <- function(w0.mut, w0.res, wa, wt.res) {
+    gam <- (((wa/wt.res)^0.25 * (1 - (w0.res/w0.mut)^0.25) + (wa/w0.mut)^0.25 - g^0.25)/((wa/w0.res)^0.25 -
+        g^0.25))^4
+
+    if (RGR_switch == 1) {
+        gam <- 0 * gam + 1
+    }
+    return(gam)
+
+}
+
+# for calculation of ESS R0 function - fitness
+R0 <- function(w0.mut, w0.res, wa, lri) {
+    return(n(w0.mut, lri) * s_mut(wa, w0.mut, w0.res, wa, lri))
+}
+
+# selection gradient
+dR0 <- function(w0, wa, lri) {
+    return((R0(w0 * (1 + DERIV), w0, wa, lri) - R0(w0 * (1 - DERIV), w0, wa, lri))/(w0 *
+        2 * DERIV))
+}
+
+# solve for ESS with analyictal solution and adult size scaling
+ESS_analytic <- function(wa, lri) {
+    return(exp(2 * (r - q) * (1 - r)/r/alph_k/(q - 1)) * (s * lri/wa^q)^(1/(1 - q)))
+}
+
+# solve for ESS by locating zero in selection gradient
+ESS_numeric <- function(wa, lri, min) {
+    Sol <- 0 * wa
+
+    for (i in 1:length(wa)) {
+        # do for all values of wa if a vector
+
+        Sol[i] <- uniroot(dR0, wa[i] * c(min, 1), tol = TOL, wa = wa[i], lri = lri[i])$root
+    }
+    return(Sol)
+}
+
+# routine for solving ESS through impelementing canon equ
+ESS_numeric2 <- function(wa, lri, delta, max_counts) {
+    res <- delta * wa
+    for (i in 1:length(wa)) {
+        # do for all values of wa if a vector
+
+        count <- 0
+        mult <- 0.5
+        der <- dR0(res[i], wa[i], lri[i])
+        # check to see if optimum is greater or lower seed size increase seed mass
+        if (der > 0) {
+            flag <- 1
+        } else {
+            # decrease seed mass
+            flag <- -1
+        }
+        # increment until opitmum located
+        while (abs(der) > TOL && count < max_counts) {
+            W0_next <- res[i] * (1 + flag * mult)
+
+            der <- dR0(W0_next, wa[i], lri[i])
+
+            # print( c(count, res, dR0(res[i], wa[i], lri[i]), mult)) increment too large -
+            # overshoots optimum
+            if (der * flag < 0 || W0_next > wa[i]) {
+
+                mult <- mult * 0.5  # decrease increment
+            } else {
+                res[i] <- W0_next
+            }
+            count <- 1 + count
+
+        }
+        print(count)
+
+    }
+    return(res)
+
+}
+
+# competition effect
+alpha_ij <- function(w1, w2) {
+    return(alph_c/(1 + (w1/w2)^alph_k))
+}
+
+# plot survival curves Create a plot of A-Ci curves showing Aj and Av
+Plot.Surv <- function(w0, wa, lri) {
+    WT <- wt(w0, wa, lri)
+
+    plot(function(x) s_est_phase(x, w0 = w0), w0, D * w0, lwd = 2, col = "red", xlab = "W (kg) [log]",
+        xlim = c(w0/2, wa * 2), ylim = c(1e-04, s * 2), ylab = "Survival (0-1)",
+        log = "xy")
+    curve(s_densindep_phase(x, w0 = w0), from = D * w0, to = max(min(WT, wa), D *
+        w0), lwd = 2, col = "blue", add = TRUE)
+    curve(s_ST_bound(x, w0 = w0, wa = wa, lri = lri), from = min(max(WT, D * w0),
+        wa), to = wa, lwd = 2, col = "green", add = TRUE)
+    curve(s_mut(x, w0.mut = w0, w0.res = w0, wa = wa, lri = lri), from = w0, to = wa,
+        lwd = 1, col = "black", add = TRUE)
+    # points(c(W0, WA), c(s_est_phase(w0,w0), s_combined(wa, w0,w0,wa)), type = 'p',
+    # cex = 1.5, ) plot(c(W0, WT, WA), c(s_est_phase(W0,W0), s_thinning_phase(WT,
+    # W0,W0,WA), s_thinning_phase(WA, W0,W0,WA)), type = 'p', cex = 1.5, log='xy')
+    legend("bottomleft", leg = c("establishment", "dens indep", "thinning", "all"),
+        lwd = c(2, 2, 2, 1), col = c("red", "blue", "green", "black"))
+}
+
+
+# log-log plot with legend and 1:1
+ll_plot <- function(X, Y, XLAB, YLAB) {
+    plot(X, Y, log = "xy", xlab = XLAB, ylab = YLAB)
+    fit <- slope.test(log10(Y), log10(X), test.value = 1, method = "SMA")
+    Int <- 10^(mean(log10(Y)) - fit$b * mean(log10(X)))
+    curve(Int * x^fit$b, min(X), max(X), add = TRUE, lwd = 2)
+    r2 <- cor(log10(Y), log10(X))^2
+    n <- length(X)
+    leg <- c(paste("B = ", format(fit$b, digits = 4)), paste("Int = ", format(Int,
+        digits = 4)), paste("n = ", n), paste("R2 = ", format(r2, digits = 3)))
+    legend(x = "topleft", legend = leg, lty = 0, cex = 1)
+}
+
+# PIP
+require(lattice)  # for 3D plotting PIP
+PIP <- function(low, up, wa) {
+    v <- seq.log(low, up, (up/low)^0.05)
+
+    N <- length(v)
+
+    x <- matrix(rep(v, time = N), ncol = N, nrow = N)
+    y <- t(x)
+
+    r <- 0 * x
+
+    for (i in 1:N) {
+        for (j in 1:N) {
+            r[i, j] <- max(0, R0(x[i, j], y[i, j], wa, LRI(wa)) - 1)
+        }
+    }
+    wireframe(r ~ log10(x) * log10(y), scales = list(arrows = FALSE), perspective = F,
+        drape = T, colorkey = TRUE, screen = list(z = 90, x = 0, y = 0))
+}
+
---
+++ analysis.R
@@ -0,0 +1,238 @@
+# Daniel Falster - offspring size model last modified Dec 2007
+
+# ---------------------------------------------------------- SETUP: load
+# libraries, set working directory
+require(smatr)  # for SMA line-fitting
+setwd("/Users/dfalster/Ecology/_Games/offspring size/model")  # setwd('F:\\_Games\\offspring size\\model')
+rm(list = ls(all = TRUE))  # clear memory
+source("R/ESS_functions_11.r")  # load sub routines
+
+# ---------------------------------------------------------- load data
+raw.mam <- read.table("../data/mammals.txt", header = T, sep = "\t")
+
+obs.mam <- subset(raw.mam, !is.na(raw.mam$Wa) & !is.na(raw.mam$W0))
+
+raw.plt <- read.table("../data/Plants.txt", header = T, sep = "\t")
+obs.plt <- subset(raw.plt, !is.na(raw.plt$Wa) & !is.na(raw.plt$W0))
+
+Wa.range.mam <- c(min(obs.mam$Wa), max(obs.mam$Wa))
+
+Wa.range.plt <- c(min(obs.plt$Wa), max(obs.plt$Wa))
+
+# ---------------------------------------------------------- Parameters
+q <- 0.2  # density independent mortality
+r <- 0.75  # thinning mortality
+k <- 10^-8
+alph_c <- 2
+alph_k <- 3  # for assymetric competition model
+RGR_switch <- 1
+# 1 = assume constant RGR
+g <- 0.8
+# defines fraction of max size for maturity (only needed if RGR_switch !=1)
+
+# Plants
+D <- 2
+c_LRI <- 0.895/35
+b_LRI <- 0.6322944
+s <- 0.00677  # optimal fit
+# Mammals
+D <- 1
+c_LRI <- 0.895/20
+b_LRI <- 0.9519
+s <- 0.944
+# optimal fit
+# -------------------------------------------------------------------------------------
+source("R/Fitted_lines_version 2.r")
+source("R/Smith-Fretwell_curve 2.r")
+source("R/Competitive_asym.r")
+source("R/Fig 6- slope_change.R")
+# -------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
+
+# MODEL ANALYSIS Check analytic solution against 2 numeric methods
+plot(ESS_analytic(obs.mam$Wa, LRI(obs.mam$Wa)), ESS_numeric(obs.mam$Wa, LRI(obs.mam$Wa),
+    1e-05), log = "xy", type = "b")
+
+curve(1 * x, from = min(obs.mam$W0), to = max(obs.mam$W0), add = T, col = "red")
+
+plot(ESS_analytic(obs.mam$Wa, LRI(obs.mam$Wa)), ESS_numeric2(obs.mam$Wa, LRI(obs.mam$Wa),
+    1e-05, 600), log = "xy", type = "b")
+
+curve(1 * x, from = min(obs.mam$W0), to = max(obs.mam$W0), add = T, col = "red")  # -------------------------------------------------------------------------------------# Compare with constant and declining RGR=1
+D <- 1
+RGR_switch <- 0
+
+Wa <- obs.mam$Wa
+# compare predictions from two models
+plot(Wa, ESS_analytic(Wa, LRI(Wa)), type = "l", log = "xy")
+
+points(Wa, ESS_numeric(Wa, LRI(Wa), 1e-04), type = "l", log = "xy", col = "red")
+# investigate change in slope as function B and competitive assymetry
+Wa <- c(mean(obs.mam$Wa), max(obs.mam$Wa))
+plot(c(0, 1), c(0, 1), type = "n", xlim = c(0, 1), ylim = c(-1, 0), xlab = "Slope with constant RGR",
+    ylab = "Slope difference")
+for (alph_k in c(0.5, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024)) {
+    X <- NULL
+    Y <- NULL
+    i <- 1
+
+    for (b_LRI in seq(1, 0.1, -0.01)) {
+        Y[i] <- slope.test(log10(ESS_numeric(Wa, LRI(Wa), k)), log10(Wa), test.value = 1,
+            method = "SMA")$b
+
+        X[i] <- (b_LRI - q)/(1 - q)
+
+        i <- i + 1
+
+    }
+    points(X, Y - X, type = "l")
+}
+points(X, 0 * X, type = "l", lty = "dashed")  # -------------------------------------------------------------------------------------
+
+# SOLVE ESS plot observed data
+Wa <- obs.mam$Wa
+W0 <- obs.mam$W0
+
+ll_plot(Wa, W0, "Adult size", "Offspring size")
+
+# compare observed vs predicted offspring size
+W0_pred <- ESS_analytic(Wa, LRI(Wa))
+
+ll_plot(W0, W0_pred, "Observed", "Predicted")
+points(W0, W0, type = "l")
+
+points(W0, W0_pred, type = "p", col = "blue")
+
+
+# find optimum fit for slope
+W0_pred <- ESS_analytic(Wa, LRI(Wa))
+
+slope <- slope.test(log10(W0_pred), log10(W0), test.value = 1, method = "MA")$b
+count <- 1
+mult <- 0.1
+
+# choose to incraese or decrease slope
+flag <- 1
+if (slope > 1) {
+    flag <- -1
+}
+while (abs(slope - 1) > 10^-3 && count < 100) {
+    temp <- b_LRI
+
+    b_LRI <- b_LRI * (1 + flag * mult)
+
+    W0_pred <- ESS_analytic(Wa, LRI(Wa))
+
+    slope <- slope.test(log10(W0_pred), log10(W0), test.value = 1, method = "MA")$b
+    print(c(count, temp, b_LRI, slope, mult))
+
+    if ((slope - 1) * flag > 0) {
+
+        # increment too large, overshoots optimum
+        mult <- mult * 0.5
+        b_LRI <- temp
+    } else count <- 1 + count
+}
+ll_plot(W0, W0_pred, "Observed", "Predicted")
+
+# find optimum fit for intercept
+W0_pred <- ESS_analytic(Wa, LRI(Wa))
+
+slope <- slope.test(log10(W0_pred), log10(W0), test.value = 1, method = "MA")$b
+int <- 10^(mean(log10(W0_pred)) - slope * mean(log10(W0)))
+count <- 1
+mult <- 0.5
+
+# choose to incraese or decrease INTERCEPT
+flag <- 1
+if (int > 1) {
+    flag <- -1
+}
+while (abs(int - 1) > 10^-3 && count < 100) {
+    temp <- s
+
+    s <- s * (1 + flag * mult)
+
+    pred2 <- ESS_analytic(Wa, LRI(Wa))
+
+    slope <- slope.test(log10(pred2), log10(W0), test.value = 1, method = "MA")$b
+    int <- 10^(mean(log10(pred2)) - slope * mean(log10(W0)))
+    print(c(count, temp, s, int, mult))
+
+    if ((int - 1) * flag > 0) { # increment too large,overshoots optimum
+        mult <- mult * 0.5
+        s <- temp
+    } else {
+        W0_pred <- pred2
+    }
+    count <- 1 + count
+
+}
+ll_plot(W0, W0_pred, "Observed", "Predicted")
+
+# other properties at ESS
+W0_pred <- ESS_analytic(Wa, LRI(Wa))
+
+
+# Plot survival curves for ESS offsrping size species by species
+Obs <- obs.mam
+
+pdf("survival.pdf", width = 6, height = 6, onefile = TRUE)
+for (i in 1:length(Obs$Wa)) {
+    Plot.Surv(W0_pred[i], Wa[i], LRI(Wa[i]))
+
+    legend("bottomright", leg = c(paste(as.character(Obs$Genus[i]), " ", as.character(Obs$species[i])),
+        paste("Wa = ", format(Obs$Wa[i], digits = 2), ", ", "Ea = ", format(Obs$Ea[i],
+            digits = 2)), paste("W0 = ", format(Obs$W0[i], digits = 2), " / ", format(W0_pred[i],
+            digits = 2)), paste("Wt = ", format(wt(W0_pred[i], Obs$Wa[i], LRI(Obs$Wa[i])),
+            digits = 2))))
+
+}
+dev.off()
+
+# ------------------------------------------------------------------------------
+# CHECKING THAT ESS IS INDEED ESS
+W0_pred <- ESS_analytic(Wa, LRI(Wa))
+
+# plot fitness function for given species
+i <- 300
+
+plot(function(x) R0(x, w0.res = W0_pred[i], wa = Wa[i], LRI(Wa[i])), min(W0), max(W0),
+    log = "x", xlab = "R0(mut, res)", ylab = "R0")
+# make pairwise inavasibility plot ??????
+
+# ------------------------------------------------------------------------------
+# BASIC MODEL OUTPUT seed production v body size
+W0 <- W0.init
+
+plot(function(x) n(W0, LRI(x)), Wa.min, Wa.max, col = 1, lwd = 1.5, log = "xy", xlim = c(Wa.min *
+    0.75, Wa.max * 1.25), xlab = "Adult size (kg) [log scale]", ylab = "Lifetime seed prod (life-1) [log scale]")
+curve(n(2 * W0, LRI(x)), min(Obs$Wa), max(Obs$Wa), add = TRUE, col = "blue")
+# seed production v seed size
+WA <- 10
+
+plot(function(x) n(x, LRI(WA)), W0.min, W0.max, col = 1, lwd = 1.5, log = "xy", xlim = c(W0.min *
+    0.75, W0.max * 1.25), xlab = "Offspring size (kg) [log scale]", ylab = "Lifetime seed prod (life-1) [log scale]")
+curve(n(x, LRI(WA/2)), min(Obs$Wa), max(Obs$Wa), add = TRUE, col = "blue")
+# Survival curves
+WA <- 10
+
+Plot.Surv(0.01, WA, LRI(WA))
+
+# Wt vs body size
+plot(function(x) wt(W0, x, LRI(x)), Wa.min, Wa.max, col = 1, lwd = 1.5, log = "xy",
+    xlim = c(Wa.min * 0.75, Wa.max * 1.25), xlab = "Adult size (kg) [log scale]",
+    ylab = "Size at onset of self-thinning(kg)[log scale]")
+
+# Wt vs W0 ll_plot(W0, wt(W0, Wa, lri), 'Offspring size (kg)', 'Size at onset
+# thinning# (kg)')
+ll_plot(W0, wt(W0, Wa, Lri_dat), "Offspring size (kg)", "Size at onset thinning (kg)")
+curve(1 * x, min(Obs$W0), max(Obs$W0), add = TRUE)
+
+# plot competition function
+plot(function(x) alpha_ij(x, 1), 0.1, 10, log = "x", xlab = "W1/W2", ylab = "a(ij) [log]")
+for (i in 1:8) {
+    alph_k <- i
+    curve(alpha_ij(x, 1), 0.1, 10, add = TRUE)
+}
